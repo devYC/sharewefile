740715f20a065983f0b112ef742fa281c07bb76e
yoonchae <jeongyooonchae@gmail.com>
2023-04-26 23:11:50 +0900

    feat: 파일 전송 및 다운로드 기능
    - 파일 전송받는 로직
    - 전송받은 파일 다운로드 로직
    - 받은 파일 정보 모달에 연결
    - 파일 다운로드 시 화살표 효과

---
+++ src/common/components/modal/transferingline/Index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+function TransferingLine() {
+  return <MovingLine />;
+}
+
+export default TransferingLine;
+
+const move = keyframes`
+from {
+  transform: rotate(-145deg) translate(0, 0);
+}
+to {
+  transform: rotate(-145deg) translate(-100%, 100%);
+}
+`;
+
+const MovingLine = styled.div`
+  position: absolute;
+  width: 50px;
+  height: 0;
+  left: 35vw;
+  top: 40vh;
+  border: 3px dashed rgba(255, 255, 255, 0.8);
+  animation: ${move} 1s linear infinite;
+
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 0;
+    height: 0;
+    top: -14px;
+    right: 50px;
+    border-top: 15px solid transparent;
+    border-left: 15px solid rgba(255, 255, 255, 0.8);
+    border-bottom: 15px solid transparent;
+    transform: rotate(-180deg);
+  }
+`;
