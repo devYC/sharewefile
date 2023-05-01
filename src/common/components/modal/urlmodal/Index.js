d188c3c26284b63d739b31c00a7706c79ef77963
devYC <devyoonchae@gmail.com>
2023-05-01 11:14:08 +0900

    feat: 파일 전송 시작 버튼

--- src/common/components/modal/urlmodal/Index.js
+++ src/common/components/modal/urlmodal/Index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import Proptypes from "prop-types";
 
 function UrlModal({ onClose, randomUrl }) {
   const [description, setDescription] = useState("Click this URL");
   const URLaddress = `http://192.168.219.103:3000/${randomUrl}`;
+  const [showStartButton, setShowStartButton] = useState(false);
 
   const handleCopyUrl = async () => {
     if (navigator.clipboard) {
@@ -17,6 +18,20 @@ function UrlModal({ onClose, randomUrl }) {
     }
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowStartButton(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const handleClickStart = () => {
+    window.location.href = "/sender";
+  };
+
   return (
     <Wrapper>
       <URLwrapper onClick={handleCopyUrl}>
@@ -25,6 +40,16 @@ function UrlModal({ onClose, randomUrl }) {
       </URLwrapper>
       <Words>Share this URL to other user</Words>
       <CloseBtn onClick={onClose}>X</CloseBtn>
+      {showStartButton && (
+        <>
+          <StartButton type="button" onClick={handleClickStart}>
+            Start
+          </StartButton>
+          <Description>
+            If receiver is ready, please click this Start button to share files!
+          </Description>
+        </>
+      )}
     </Wrapper>
   );
 }
@@ -121,6 +146,10 @@ const URL = styled.div`
     font-weight: 700;
     font-style: italic;
   }
+
+  @media only screen and (max-device-width: 768px) and (-webkit-min-device-pixel-ratio: 2) {
+    line-height: 1.3em;
+  }
 `;
 
 const URLwrapper = styled.div`
@@ -132,6 +161,11 @@ const URLwrapper = styled.div`
   top: 14vh;
   border: 0.01em solid #999999;
   border-radius: 2em;
+
+  @media only screen and (max-device-width: 768px) and (-webkit-min-device-pixel-ratio: 2) {
+    top: 40%;
+    height: 17%;
+  }
 `;
 
 const CloseBtn = styled.span`
@@ -148,6 +182,36 @@ const CloseBtn = styled.span`
   line-height: 1.3em;
   padding: 0.5em;
   color: #000000;
+
+  @media only screen and (max-device-width: 768px) and (-webkit-min-device-pixel-ratio: 2) {
+    top: 1vh;
+  }
+`;
+
+const StartButton = styled.button`
+  position: absolute;
+  font-size: 1.2em;
+  top: 36vh;
+  left: 4vw;
+  transform: translate(-50%, -50%);
+  width: 20vw;
+  height: 20vh;
+  max-width: 5vw;
+  max-height: 5vh;
+  border-radius: 50px;
+  border: none;
+  appearance: none;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  @media only screen and (max-device-width: 768px) and (-webkit-min-device-pixel-ratio: 2) {
+    top: 22.5vh;
+    left: 4vw;
+    max-width: 8vw;
+    max-height: 2.5vh;
+    border-radius: 25px;
+  }
 `;
 
 UrlModal.propTypes = {
